287a703ddaba17714ee368a9b7731fc974ab935b
YargerJon-FS <jlyarger@student.fullsail.edu>
2022-07-25 08:52:23 -0400

    added validation

--- api/models/movie.js
+++ api/models/movie.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose");
 
 const movieSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    title: String,
-    director: String
+    title: {
+        type: String, 
+        required: true
+    },
+    director: {
+        type: String, 
+        required: true
+    }
 });
 
 module.exports =mongoose.model("Movie", movieSchema);
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = express.Router();
-const Movie= require("../models/movies");
+const Movie= require("../models/movie");
 
 router.get("/",(req,res,next)=> {
     res.json({message:"Movies - GET"
@@ -10,44 +10,64 @@ router.get("/",(req,res,next)=> {
 
 router.get("/:movieId",(req,res,next)=> {
     const movieId= req.params.movieId;
-    res.json({message:"Moviess - GET",
+    res.json({message:"Movies - GET",
     id: movieId
     });
 });
 
 router.post("/",(req,res,next)=> {
-    
-const newMovie = new Movie({
-    _id: mongoose.Types.ObjectId(),
-    title: req.body.title,
-    director: req.body.director,
-});
-
-//write to the database
-newMovie.save()
-.then(result => {
-    console.log(result);
-    res.status(200).json({
-        message: "Movie Saved",
-        book:{
-            title: result.title,
-            director: result.director,
-            id: result._id,
-            metadata:{
-                method: req.method,
-                host: req.hostname
+        Movie.find({
+            title: req.body.title, 
+            director: req.body.director
+        })
+        .exec()
+        .then(result =>{
+            console.log(result);
+            if(result.lenth>0){
+                return res.status(406).json({
+                    message: "Movie is already cataloged"
+                })
             }
-        }
-    });
-})
-.catch(err => {
-    console.error(err.message);
-    res.status(500).json({
-        error: {
-            message: err.message
-        }
-    });
-});
+            const newMovie = new Movie({
+                _id: mongoose.Types.ObjectId(),
+                title: req.body.title,
+                director: req.body.director,
+            });
+            
+            //write to the database
+            newMovie.save()
+            .then(result => {
+                console.log(result);
+                res.status(200).json({
+                    message: "Movie Saved",
+                    movie:{
+                        title: result.title,
+                        director: result.director,
+                        id: result._id,
+                        metadata:{
+                            method: req.method,
+                            host: req.hostname
+                        }
+                    }
+                });
+            })
+            .catch(err => {
+                console.error(err.message);
+                res.status(500).json({
+                    error: {
+                        message: err.message
+                    }
+                });
+            });
+        })
+        .catch(err =>{
+            console.log(error);
+            res.status(500).json({
+                error:{
+                    message: "Unabled to save movie with title" + req.body.title
+                }
+            })
+        })
 
 });
 
